ae2a3932594b740551546bc73c87093bc3d2da56
Dustin Reed <dmreedy1@gmail.com>
2016-10-16 21:49:17 -0500

    set spell and class to player.  add start to battle text

--- javascripts/player.js
+++ javascripts/player.js
@@ -11,6 +11,7 @@ player.Combatants.Player = function(name) {
   this.species = null;
   this.class = null;
   this.weapon = null;
+  this.spell = null;
 
   this.playerName = name || "unknown adventurer";
   this.health = Math.floor(Math.random() * 40 + 50);
@@ -43,15 +44,27 @@ player.Combatants.Player.prototype.setWeapon = function(newWeapon) {
   this.weapon = newWeapon;
 };
 
+player.Combatants.Player.prototype.setSpell = function(newSpell) {
+  this.spell = newSpell;
+};
+
 player.Combatants.Player.prototype.generateClass = function() {
   // Get a random index from the allowed classes array
   var random = Math.round(Math.random() * (this.allowedClasses.length - 1));
 
   // Get the string at the index
   var randomClass = this.allowedClasses[random];
-
+  console.log('Random Class', randomClass);
   // Composes the corresponding player class into the player object
   this.class = new Gauntlet.GuildHall[randomClass]();
+
+  // Add the health bonus
+  this.health += this.class.healthBonus;
+  return this.class;
+};
+
+player.Combatants.Player.prototype.setClass = function(newClass) {
+  this.class = new Gauntlet.GuildHall[newClass]();
 
   // Add the health bonus
   this.health += this.class.healthBonus;
